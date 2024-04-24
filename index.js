055fa8d44af91ed3d47320b24e0f88d462c4e8bc
Aishwarya Sharma <roli.mradul@gmail.com>
2024-04-24 21:17:04 +0530

    mealDiscount update

--- index.js
+++ index.js
@@ -2,22 +2,24 @@ import { menuArray } from "/data.js";
 
 const checkoutItems = document.getElementById("inner-checkout-container");
 const showTotalPrice = document.getElementById("total-price");
+const showFinalPrice = document.getElementById("final-price");
+const showDiscountPrice = document.getElementById("discount-price");
 let orderArray = [];
 const completeBtn = document.getElementById("completeBtn");
 const modal = document.getElementById("modal");
 const cardForm = document.getElementById("card-form");
 const thanksDiv = document.getElementById("thanks-div");
+const discountCont = document.getElementById("discount-container");
+const finalCont = document.getElementById("final-container");
 
-var acceptedCreditCards = {
-  visa: /^4[0-9]{12}(?:[0-9]{3})?$/,
-  mastercard:
-    /^5[1-5][0-9]{14}$|^2(?:2(?:2[1-9]|[3-9][0-9])|[3-6][0-9][0-9]|7(?:[01][0-9]|20))[0-9]{12}$/,
-  amex: /^3[47][0-9]{13}$/,
-  discover:
-    /^65[4-9][0-9]{13}|64[4-9][0-9]{13}|6011[0-9]{12}|(622(?:12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|9[01][0-9]|92[0-5])[0-9]{10})$/,
-  diners_club: /^3(?:0[0-5]|[68][0-9])[0-9]{11}$/,
-  jcb: /^(?:2131|1800|35[0-9]{3})[0-9]{11}$/,
-};
+// var acceptedCreditCards = {
+//   visa: /^4[0-9]{12}(?:[0-9]{3})?$/,
+//   mastercard: /^5[1-5][0-9]{14}$|^2(?:2(?:2[1-9]|[3-9][0-9])|[3-6][0-9][0-9]|7(?:[01][0-9]|20))[0-9]{12}$/,
+//   amex: /^3[47][0-9]{13}$/,
+//   discover: /^65[4-9][0-9]{13}|64[4-9][0-9]{13}|6011[0-9]{12}|(622(?:12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|9[01][0-9]|92[0-5])[0-9]{10})$/,
+//   diners_club: /^3(?:0[0-5]|[68][0-9])[0-9]{11}$/,
+//   jcb: /^(?:2131|1800|35[0-9]{3})[0-9]{11}$/
+// };
 
 const menuHtml = menuArray.map(function (foodItem) {
   return `<section>
@@ -100,8 +102,6 @@ function getOrderHtml() {
 }
 
 function handleRemoveClick(foodItemId) {
-  // console.log("index",foodItemId)
-
   // Setting show total price
   let totalPrice = Number(showTotalPrice.innerHTML);
 
@@ -109,17 +109,31 @@ function handleRemoveClick(foodItemId) {
     return index === Number(foodItemId);
   })[0];
 
-  console.log(targetFoodItemObj);
-
-  totalPrice -= targetFoodItemObj.price;
-  showTotalPrice.innerText = totalPrice;
-
   const modifiedFoodItemObj = orderArray.filter(
     (foodItem, i) => i !== Number(foodItemId)
   );
   orderArray = modifiedFoodItemObj;
 
   checkoutItems.innerHTML = getOrderHtml();
+
+  totalPrice -= targetFoodItemObj.price;
+  showTotalPrice.innerText = totalPrice;
+
+  // Checking for discount
+  let discount = checkDealDiscount();
+  if (discount !== 0) {
+    console.log(discount);
+    showDiscountPrice.innerText = discount;
+    showFinalPrice.innerText = totalPrice - discount;
+  } else {
+    showDiscountPrice.innerText = discount;
+    showFinalPrice.innerText = totalPrice;
+
+    if (!discountCont.classList.contains("hidden")) {
+      discountCont.classList.toggle("hidden");
+      finalCont.classList.toggle("hidden");
+    }
+  }
 
   const classlistCheckoutContainer = document.getElementById(
     `order-checkout-container`
@@ -138,10 +152,25 @@ function handleAddClick(foodItemId) {
 
   checkoutItems.innerHTML = getOrderHtml();
 
-  // Setting show total price
   let totalPrice = Number(showTotalPrice.innerHTML);
   totalPrice += targetFoodItemObj.price;
   showTotalPrice.innerText = totalPrice;
+
+  //Discout
+  let discount = checkDealDiscount();
+  if (discount !== 0) {
+    console.log(discount);
+    showDiscountPrice.innerText = discount;
+    showFinalPrice.innerText = totalPrice - discount;
+
+    if (discountCont.classList.contains("hidden")) {
+      discountCont.classList.toggle("hidden");
+      finalCont.classList.toggle("hidden");
+    }
+  } else {
+    // Setting show final price
+    showFinalPrice.innerText = totalPrice;
+  }
 
   // adding/removing hidden class
   const classlistCheckoutContainer = document.getElementById(
@@ -152,35 +181,67 @@ function handleAddClick(foodItemId) {
   }
 }
 
-function validateCard(value) {
-  // remove all non digit characters
-  var value = value.replace(/\D/g, "");
-  var sum = 0;
-  var shouldDouble = false;
-  // loop through values starting at the rightmost side
-  for (var i = value.length - 1; i >= 0; i--) {
-    var digit = parseInt(value.charAt(i));
+function checkDealDiscount() {
+  let pizzaCount = 0;
+  let burgerCount = 0;
+  let beerCount = 0;
+  let discount = 0;
 
-    if (shouldDouble) {
-      if ((digit *= 2) > 9) digit -= 9;
-    }
-
-    sum += digit;
-    shouldDouble = !shouldDouble;
-  }
-
-  var valid = sum % 10 == 0;
-  var accepted = false;
-
-  // loop through the keys (visa, mastercard, amex, etc.)
-  Object.keys(acceptedCreditCards).forEach(function (key) {
-    var regex = acceptedCreditCards[key];
-    if (regex.test(value)) {
-      accepted = true;
+  orderArray.forEach(function (item) {
+    if (item.id === 0) {
+      pizzaCount++;
+    } else if (item.id === 1) {
+      burgerCount++;
+    } else if (item.id === 2) {
+      beerCount++;
     }
   });
 
-  return valid && accepted;
+  console.log("Pizza count - " + pizzaCount);
+  console.log("Burger count - " + burgerCount);
+  console.log("Beer count - " + beerCount);
+
+  if (pizzaCount && burgerCount && beerCount) {
+    let count = Math.min(pizzaCount, Math.min(burgerCount, beerCount));
+    discount = count * 2;
+  }
+
+  if (discount !== 0) {
+    return discount;
+  } else {
+    return 0;
+  }
 }
+
+// function validateCard(value) {
+//   // remove all non digit characters
+//   var value = value.replace(/\D/g, '');
+//   var sum = 0;
+//   var shouldDouble = false;
+//   // loop through values starting at the rightmost side
+//   for (var i = value.length - 1; i >= 0; i--) {
+//     var digit = parseInt(value.charAt(i));
+
+//     if (shouldDouble) {
+//       if ((digit *= 2) > 9) digit -= 9;
+//     }
+
+//     sum += digit;
+//     shouldDouble = !shouldDouble;
+//   }
+
+//   var valid = (sum % 10) == 0;
+//   var accepted = false;
+
+//   // loop through the keys (visa, mastercard, amex, etc.)
+//   Object.keys(acceptedCreditCards).forEach(function(key) {
+//     var regex = acceptedCreditCards[key];
+//     if (regex.test(value)) {
+//       accepted = true;
+//     }
+//   });
+
+//   return valid && accepted;
+// }
 
 document.getElementById("inner-container").innerHTML = menuHtml.join("");
